7fb9e8b35c1043e0f8f150a10a4e340a82b30071
Ned Fenstermacher <nedfenstermacher@gmail.com>
2018-04-15 16:45:58 -0400

    Add links for Laravel file manager.

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,6 +5,8 @@
 @section('content')
     <h1>Admin</h1>
     <a href="/posts/create" class="btn btn-primary mb-4">Create Post</a>
+    <a href="/bin?type=images" class="btn btn-primary mb-4">Manages Image</a>
+    <a href="/bin?type=files" class="btn btn-primary mb-4">Manage Files</a>
     <ul class="list-group">
         @foreach($posts as $post)
             <li class="list-group-item">
