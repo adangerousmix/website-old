17773492fe01133a17c3c02612a58309246374e2
Ned Fenstermacher <naf12@psu.edu>
2018-08-27 21:40:41 -0400

    Update look.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,31 +15,68 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <link rel="icon" 
+      type="image/png" 
+      href="adm.ico">
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700|Oswald" rel="stylesheet">
+
     <!-- Styles -->
     <link rel="stylesheet" href="/css/app.css">
     @yield('styles')
 </head>
 <body>
-    <header class="container text-center mb-4">
-        <a href="/"><img src="/images/shares/adm_logo.png" alt="A Dangerous Mix Logo" class="logo p-4"></a>
-        <nav class="mt-4">
-            <ul class="nav justify-content-center">
-                <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
-                <li class="nav-item"><a class="nav-link" href="/podcast">Podcast</a></li>
-                <li class="nav-item"><a class="nav-link" href="/comics">Comics</a></li>
-                <li class="nav-item"><a class="nav-link" href="/movies">Movies</a></li>
-                <li class="nav-item"><a class="nav-link" href="/tv">TV</a></li>
-                <li class="nav-item"><a class="nav-link" href="/about">About</a></li>
-                @if (Auth::check())
-                    <li class="nav-item"><a class="nav-link" href="/posts">Admin</a></li>
-                    <li class="nav-item"><a class="nav-link" href="/logout">Logout</a></li>
-                @endif
-            </ul>
+    <header class="row container mx-auto">
+        <div class="col-md-12 text-center my-4">
+            <a href="/">
+                <img src="/images/shares/adm_logo.png" alt="A Dangerous Mix Logo" class="logo align-top pt-1">
+            </a>
+        </div>
+        <nav class="navbar navbar-expand-sm navbar-light text-center col-md-12 mb-4" style="background-color: #ffffff;">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse text-center" id="navbarTogglerDemo01">
+                <ul class="navbar-nav mx-auto mt-2 mt-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="/">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/podcast">Podcast</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/comics">Comics</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/movies">Movies</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/tv">TV</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/about">About</a>
+                    </li>
+                    @if (Auth::check())
+                    <li class="nav-item">
+                        <a class="nav-link" href="/posts">Posts</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/logout">Logout</a>
+                    </li>
+                    @endif
+                </ul>
+                <!-- <form class="form my-2 my-lg-0 w-100">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn my-2 my-sm-0" type="submit">Search</button>
+                </form> -->
+            </div>
         </nav>
     </header>
-    <div class="container">
-        <main role="main">
+    <div>
+        <main role="main" class="container">
             @yield('content')
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea maxime eum recusandae incidunt temporibus magnam? Expedita ut, accusantium dolor quos labore natus saepe distinctio vitae at eligendi, voluptatibus neque quod.</p>
         </main>
     </div>
     <footer class="container text-center mt-4">
