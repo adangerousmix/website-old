473575e3d4bc5fd157273e907e4049688ce15b35
Ned Fenstermacher <nedfenstermacher@gmail.com>
2018-09-08 19:31:00 -0400

    Update about and show page.

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -4,14 +4,17 @@
 
 @section('content')
     <h1>About</h1>
-    <p>A Dangerous Mix is ran by one man who enjoys various types of entertainment.</p>
+    <p>A Dangerous Mix is ran by one man who's a simple fan of many various types of entertainment.</p>
     <h2>Social Links</h2>
     <ul>
         <li><a href="https://facebook.com/adangerousmix">Facebook</a></li>
+        <li><a href="https://www.instagram.com/adangerousmix">Instagram</a></li>
+        <li><a href="https://www.twitch.tv/adangerousmix">Twitch</a></li>
         <li><a href="https://twitter.com/adangerousmix">Twitter</a></li>
     </ul>
     <h2>Recommended Links</h2>
     <ul>
         <li><a href="https://batman-news.com/">Batman News</a></li>
+        <li><a href="https://www.dcuniverse.com/">DC Universe</a></li>
     </ul>
 @endsection
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,6 +3,7 @@
 @section('title', $post->title)
 
 @section('content')
+    <img src="{{ $post->image }}" class="card-img-top" alt="{{ $post->title }}">
     <h1>{{ $post->title }}</h1>
     {!! Markdown::convertToHtml($post->body) !!}
 @endsection
