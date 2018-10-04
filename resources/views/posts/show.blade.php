0f850e8b24e8c42ac4b1dd06e5a77bca877ea891
Ned Fenstermacher <naf12@psu.edu>
2018-10-03 22:36:04 -0400

    Added whitespace.

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,11 +4,12 @@
 
 @section('content')
     <img src="{{ $post->image }}" class="card-img-top" alt="{{ $post->title }}">
-    <h1>{{ $post->title }}</h1>
+    <h1 class="mt-4">{{ $post->title }}</h1>
+    <small class="text-muted mb-4 d-block">{{ $post->category }} - {{ $post->created_at }}</small>
     {!! Markdown::convertToHtml($post->body) !!}
-    <p>Tags: 
+    <p class="mt-4">Tags: 
         @foreach($post->tags->pluck('name')->all() as $tag)
-            <small class="mr-2"><a href="/tag/{{ $tag }}">{{ $tag }}</a></small>
+            <span class="mr-2"><a href="/tag/{{ $tag }}">{{ $tag }}</a></span>
         @endforeach
     </p>
 @endsection
